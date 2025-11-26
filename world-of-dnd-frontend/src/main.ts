cc786057acd64d62e57ae3ba7b894e5d799d5602
Luigi Balotta <l.balotta@mindicity.com>
2025-11-26 14:32:44 +0100

    Try to fix leaflet in production

--- world-of-dnd-frontend/src/main.ts
+++ world-of-dnd-frontend/src/main.ts
@@ -1,5 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
+import 'leaflet';
+import 'leaflet-draw';
+
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+    .catch(err => console.error(err));
