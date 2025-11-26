e411f9e7d3120377ab546f5f68685e6ea0b01d86
Luigi Balotta <l.balotta@mindicity.com>
2025-11-26 16:02:24 +0100

    Try again to fix leaflet pt8

--- world-of-dnd-frontend/src/main.ts
+++ world-of-dnd-frontend/src/main.ts
@@ -1,8 +1,10 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import * as L from 'leaflet';
+// Rende L globale così i plugin (leaflet-draw) possono usarla
 (window as any).L = L;
 import 'leaflet-draw';
+import 'leaflet-draw/dist/leaflet.draw.css';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
     .catch(err => console.error(err));
