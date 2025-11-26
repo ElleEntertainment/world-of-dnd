8ee05107ee48ee13acde60f0d60a5ddd28e12a5d
Luigi Balotta <l.balotta@mindicity.com>
2025-11-26 15:55:43 +0100

    Try again to fix leaflet pt7

--- world-of-dnd-frontend/src/main.ts
+++ world-of-dnd-frontend/src/main.ts
@@ -3,8 +3,6 @@ import { AppModule } from './app/app.module';
 import * as L from 'leaflet';
 (window as any).L = L;
 import 'leaflet-draw';
-import 'leaflet-draw/dist/leaflet.draw.js';
-import 'leaflet-draw/dist/leaflet.draw.css';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
     .catch(err => console.error(err));
